0ac40753df1f34b816b0a11fb537ab1357125e95
mamor <mamor.dev@gmail.com>
2013-12-01 16:19:17 +0900

    if.jsを追加した

---
+++ js/if.js
@@ -0,0 +1,9 @@
+var itemCount = 1;
+
+if (0 == itemCount) {
+	console.log('商品数は0');
+} else if((1 <= itemCount) && (itemCount < 5)) {
+	console.log('商品数は1以上5未満');
+} else {
+	console.log('商品数は5以上');
+}
